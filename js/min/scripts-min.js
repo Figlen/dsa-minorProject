2e475bb5740fcafe119284b3c74327eb2d8b48ea
CianR <cian.roche@webinnovate.ie>
2015-01-23 11:25:48 +0000

    tidyup

--- js/min/scripts-min.js
+++
@@ -1,1 +0,0 @@
-function checkCheck(c,e){var n=c.src;c.src=-1!=n.indexOf(e+"_checked.png")?"img/"+e+"_unchecked.png":"img/"+e+"_checked.png"}
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
+
+    // * Author: YOUR  NAME
+    // * Assignment: Minor Project – More Street
+    // * Student ID: 
+    // * Date  : 2015/10/23
+    // * Ref: Bootstrap- http://getbootstrap.com/
+    //       Font-awesome - http://fortawesome.github.io/Font-Awesome/
+    //       j-Query - http://jquery.com/
+    //       Google fonts - https://www.google.com/fonts
+  
+
+
+
 function checkCheck(id, colour) {
 	var img = id.src;
 	if (img.indexOf(colour + '_checked.png')!=-1) {
